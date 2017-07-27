4d0dfe3fe620d1b297bbb1b6c23844cdf6501290
Thomas Laegler <thomas@xmdevelopments.com>
2017-07-28 00:01:57 +1200

    Removed old projects

--- microservice-example-code2model/docker-compose-up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Trying to start Docker Compose Setup ..."
-#cd src/main/resources
-docker-compose up --no-build --remove-orphans
-#cd ../../..
-echo "Docker Compose Setup started."
